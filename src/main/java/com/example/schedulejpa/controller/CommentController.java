b55173613f0b9383357c1da0589f67c79d6076dd
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 19:22:28 +0900

    feat : 댓글 수정,삭제 API 추가

--- src/main/java/com/example/schedulejpa/controller/CommentController.java
+++ src/main/java/com/example/schedulejpa/controller/CommentController.java
@@ -7,6 +7,8 @@ import com.example.schedulejpa.dto.CommentResponseDto;
 import com.example.schedulejpa.dto.UserResponseDto;
 import com.example.schedulejpa.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -38,11 +40,38 @@ public class CommentController {
     @GetMapping
     public ResponseEntity<List<CommentResponseDto>> findAll(
             @PathVariable Long scheduleId
-    ) {
 
+    ) {
         List<CommentResponseDto> responseDtoList = commentService.findAll(scheduleId);
 
         return new ResponseEntity<>(responseDtoList,HttpStatus.OK);
 
+    }
+
+    @PatchMapping("/{commentId}")
+    public ResponseEntity<Void> update(
+            @PathVariable Long scheduleId,
+            @PathVariable Long commentId,
+            @RequestBody CommentRequestDto requestDto,
+            @SessionAttribute(name = Const.LOGIN_USER, required = false)
+            UserResponseDto loginUser
+    ) {
+
+        commentService.update(scheduleId, commentId, requestDto.getContents(), loginUser);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long scheduleId,
+            @PathVariable Long commentId,
+            @SessionAttribute(name = Const.LOGIN_USER, required = false)
+            UserResponseDto loginUser
+    ) {
+
+        commentService.delete(scheduleId, commentId, loginUser);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
