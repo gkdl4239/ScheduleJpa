095a24eaa396f620aeb7b3e8b672b1928d5ae503
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 17:14:57 +0900

    feat : 댓글 응답Dto 구현

--- src/main/java/com/example/schedulejpa/dto/CommentResponseDto.java
+++ src/main/java/com/example/schedulejpa/dto/CommentResponseDto.java
@@ -1,4 +1,33 @@
 package com.example.schedulejpa.dto;
 
+import com.example.schedulejpa.entity.Comment;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class CommentResponseDto {
+
+    private final String username;
+
+    private final String contents;
+
+    private final LocalDateTime createdAt;
+
+    private final LocalDateTime updatedAt;
+
+    public CommentResponseDto(String username, String contents, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.username = username;
+        this.contents = contents;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static CommentResponseDto toDto(Comment comment) {
+        return new CommentResponseDto(
+                comment.getUser().getUsername(),
+                comment.getContents(),
+                comment.getCreatedAt(),
+                comment.getUpdatedAt());
+    }
 }
