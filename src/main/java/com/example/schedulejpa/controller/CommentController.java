f2cdd3ab933450f498992560157ce2f115e502b0
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 17:14:08 +0900

    feat : 댓글 생성,전체조회 API 추가

--- src/main/java/com/example/schedulejpa/controller/CommentController.java
+++ src/main/java/com/example/schedulejpa/controller/CommentController.java
@@ -2,16 +2,47 @@ package com.example.schedulejpa.controller;
 
 
 import com.example.schedulejpa.common.Const;
+import com.example.schedulejpa.dto.CommentRequestDto;
 import com.example.schedulejpa.dto.CommentResponseDto;
 import com.example.schedulejpa.dto.UserResponseDto;
 import com.example.schedulejpa.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/schedules/{scheduleId}")
+@RequestMapping("/schedules/{scheduleId}/comments")
 @RequiredArgsConstructor
 public class CommentController {
+    private final CommentService commentService;
 
+    @PostMapping
+    public ResponseEntity<CommentResponseDto> save(
+            @PathVariable Long scheduleId,
+            @RequestBody CommentRequestDto requestDto,
+            @SessionAttribute(name = Const.LOGIN_USER, required = false)
+            UserResponseDto loginUser) {
+
+
+        CommentResponseDto responseDto = commentService.save(
+                scheduleId,
+                requestDto.getContents(),
+                loginUser);
+
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CommentResponseDto>> findAll(
+            @PathVariable Long scheduleId
+    ) {
+
+        List<CommentResponseDto> responseDtoList = commentService.findAll(scheduleId);
+
+        return new ResponseEntity<>(responseDtoList,HttpStatus.OK);
+
+    }
 }
