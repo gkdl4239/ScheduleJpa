6128a3ec2331cfc0cb84a0bad7c157789811ff83
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 16:16:08 +0900

    feat : 댓글 컨트롤러 생성

---
+++ src/main/java/com/example/schedulejpa/controller/CommentController.java
@@ -0,0 +1,17 @@
+package com.example.schedulejpa.controller;
+
+
+import com.example.schedulejpa.common.Const;
+import com.example.schedulejpa.dto.CommentResponseDto;
+import com.example.schedulejpa.dto.UserResponseDto;
+import com.example.schedulejpa.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/schedules/{scheduleId}")
+@RequiredArgsConstructor
+public class CommentController {
+
+}
