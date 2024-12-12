9e6c03b29b7c815809f11cee3578b791d66ef444
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 10:14:37 +0900

    feat : 회원가입 응답 필드값 및 생성자 추가

--- src/main/java/com/example/schedulejpa/dto/SignUpResponseDto.java
+++ src/main/java/com/example/schedulejpa/dto/SignUpResponseDto.java
@@ -6,4 +6,15 @@ import lombok.Getter;
 @Getter
 public class SignUpResponseDto {
 
+    private final Long id;
+
+    private final String username;
+
+    private final String email;
+
+    public SignUpResponseDto(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
 }
