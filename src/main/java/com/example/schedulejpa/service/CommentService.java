7fcafb9877d3d3658f04339a1b2ea2b88ed28f80
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 16:16:28 +0900

    feat : 댓글 서비스 생성

---
+++ src/main/java/com/example/schedulejpa/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.example.schedulejpa.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+
+    public
+}
