0d4e0236c1a212ce1499998f70f53a6c96d9f15b
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 16:34:35 +0900

    feat : 댓글 레포지토리 생성

---
+++ src/main/java/com/example/schedulejpa/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.example.schedulejpa.repository;
+
+import com.example.schedulejpa.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
