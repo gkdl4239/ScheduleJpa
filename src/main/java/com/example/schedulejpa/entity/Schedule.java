1c086bdb3f817076dd39545201a3e7998cf7814b
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 15:14:08 +0900

    fix : 일정과 유저 다대일 관계 명시 및 생성자/Setter 생성

--- src/main/java/com/example/schedulejpa/entity/Schedule.java
+++ src/main/java/com/example/schedulejpa/entity/Schedule.java
@@ -13,12 +13,29 @@ public class Schedule extends BaseEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String username;
     private String title;
 
     @Column(columnDefinition = "longtext")
     private String contents;
 
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
+    public Schedule(String title, String contents, User user) {
+        this.title = title;
+        this.contents = contents;
+        this.user = user;
+    }
+
+     public Schedule() {
+
+    }
+
+    public void setTitleAndContents(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
 
 }
