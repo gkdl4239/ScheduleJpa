9e2dc46fb9444a03f193c76d197ce74328ed7cde
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 17:14:31 +0900

    feat : 일정의 id로 모든 댓글을 찾는 쿼리메서드 추가

--- src/main/java/com/example/schedulejpa/repository/CommentRepository.java
+++ src/main/java/com/example/schedulejpa/repository/CommentRepository.java
@@ -3,5 +3,11 @@ package com.example.schedulejpa.repository;
 import com.example.schedulejpa.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+
+    List<Comment> findByScheduleId(Long scheduleId);
+
 }
