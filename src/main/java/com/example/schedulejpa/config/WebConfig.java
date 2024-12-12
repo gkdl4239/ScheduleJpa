cfdcbc97a3dbdfebf6c39567d169baa9ae3673dd
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 15:21:09 +0900

    feat : 필터 사용을 위한 Configuration 추가

--- src/main/java/com/example/schedulejpa/config/WebConfig.java
+++ src/main/java/com/example/schedulejpa/config/WebConfig.java
@@ -1,4 +1,24 @@
 package com.example.schedulejpa.config;
 
-public class WebConfig {
+import com.example.schedulejpa.filter.LoginFilter;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean loginFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter( new LoginFilter()); // Filter 등록
+        filterRegistrationBean.setOrder(1); // Filter 순서 1 설정
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
 }
