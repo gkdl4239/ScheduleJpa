d4ac5d84c825bf88ad1bc9d02321223f8126f879
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 19:23:49 +0900

    feat : 댓글개수 연산, 반환값이 Comment인 메서드 추가

--- src/main/java/com/example/schedulejpa/repository/CommentRepository.java
+++ src/main/java/com/example/schedulejpa/repository/CommentRepository.java
@@ -2,12 +2,25 @@ package com.example.schedulejpa.repository;
 
 import com.example.schedulejpa.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
+    long countByScheduleId(Long scheduleId);
 
     List<Comment> findByScheduleId(Long scheduleId);
+
+    default Comment findByIdOrElseThrow(Long id){
+        return findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "존재하지 않는 댓글 입니다."
+                        ));
+    }
+
 
 }
