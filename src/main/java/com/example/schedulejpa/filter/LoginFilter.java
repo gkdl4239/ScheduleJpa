23086b0ab0bb8a86649f3502a3f243e142ed007a
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 15:13:35 +0900

    fix : getAttribute 의 속성이름 제대로 명시

--- src/main/java/com/example/schedulejpa/filter/LoginFilter.java
+++ src/main/java/com/example/schedulejpa/filter/LoginFilter.java
@@ -1,5 +1,6 @@
 package com.example.schedulejpa.filter;
 
+import com.example.schedulejpa.common.Const;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,7 +11,7 @@ import java.io.IOException;
 
 public class LoginFilter implements Filter {
 
-    private static final String[] WHITE_LIST = {"/", "users/signup", "/login", "/logout"};
+    private static final String[] WHITE_LIST = {"/", "/users/signup", "/users/login", "/logout"};
 
     @Override
     public void doFilter(
@@ -28,17 +29,14 @@ public class LoginFilter implements Filter {
 
             HttpSession session = httpRequest.getSession(false);
 
-            if (session == null || session.getAttribute("sessionKey값") == null) {
+            if (session == null || session.getAttribute(Const.LOGIN_USER) == null) {
                 throw new RuntimeException("로그인 해주세요");
             }
         }
-
         chain.doFilter(request, response);
-
-
     }
 
-    private boolean isWhiteList(String requestURI) {
+    private boolean isWhiteList (String requestURI){
         return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
     }
 }
