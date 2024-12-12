04e0cfd9ace13d0d55260647569c8e6b36f6f969
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 15:16:22 +0900

    feat : 일정 응답값의 필드,생성자,Setter 추가

--- src/main/java/com/example/schedulejpa/dto/ScheduleResponseDto.java
+++ src/main/java/com/example/schedulejpa/dto/ScheduleResponseDto.java
@@ -1,4 +1,27 @@
 package com.example.schedulejpa.dto;
 
+import com.example.schedulejpa.entity.Schedule;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class ScheduleResponseDto {
+
+    private String username;
+
+    private String title;
+
+    private String contents;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public static ScheduleResponseDto toDto(Schedule schedule){
+        return new ScheduleResponseDto(schedule.getUser().getUsername(), schedule.getTitle(), schedule.getContents(), schedule.getCreatedAt(), schedule.getUpdatedAt());
+    }
+
 }
