8a1d3ddc8059913166a0457dee001a3bf498b6db
gkdl4239 <gkdl4239@gmail.com>
2024-12-12 17:15:16 +0900

    feat : 댓글 서비스단에 생성과 조회 메서드 구현

--- src/main/java/com/example/schedulejpa/service/CommentService.java
+++ src/main/java/com/example/schedulejpa/service/CommentService.java
@@ -1,12 +1,49 @@
 package com.example.schedulejpa.service;
 
+import com.example.schedulejpa.dto.CommentResponseDto;
+import com.example.schedulejpa.dto.UserResponseDto;
+import com.example.schedulejpa.entity.Comment;
+import com.example.schedulejpa.entity.Schedule;
+import com.example.schedulejpa.entity.User;
+import com.example.schedulejpa.repository.CommentRepository;
+import com.example.schedulejpa.repository.ScheduleRepository;
+import com.example.schedulejpa.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
+    private final ScheduleRepository scheduleRepository;
+    private final UserRepository userRepository;
+    private final CommentRepository commentRepository;
 
-    public
+    public CommentResponseDto save(Long scheduleId, String contents, UserResponseDto loginUser) {
+
+        User user = userRepository.findByIdOrElseThrow(loginUser.getId());
+        Schedule schedule = scheduleRepository.findByIdOrElseThrow(scheduleId);
+        Comment comment = new Comment(contents, user, schedule);
+
+        Comment savedComment = commentRepository.save(comment);
+
+        return new CommentResponseDto(
+                savedComment.getUser().getUsername(),
+                savedComment.getContents(),
+                savedComment.getCreatedAt(),
+                savedComment.getUpdatedAt()
+                );
+
+    }
+
+    public List<CommentResponseDto> findAll(Long scheduleId) {
+
+        return commentRepository.findByScheduleId(scheduleId).stream()
+                .map(CommentResponseDto::toDto)
+                .toList();
+
+    }
 }
