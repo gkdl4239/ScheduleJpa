5b9e49eae696d971551723a5095c82c29bcdfce5
gkdl4239 <gkdl4239@gmail.com>
2024-12-13 17:23:11 +0900

    fix : 유저 수정을 위한 이름,비밀번호 Setter 설정

--- src/main/java/com/example/schedulejpa/entity/User.java
+++ src/main/java/com/example/schedulejpa/entity/User.java
@@ -3,6 +3,7 @@ package com.example.schedulejpa.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
@@ -15,10 +16,12 @@ public class User extends BaseEntity {
     @Id
     private Long id;
 
+    @Setter
     private String username;
 
     private String email;
 
+    @Setter
     private String password;
 
     @Transient
