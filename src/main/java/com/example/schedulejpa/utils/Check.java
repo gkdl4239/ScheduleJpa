b8ad6e44e3b84ea6a3bd1ea6fca238e6a89f19e4
gkdl4239 <gkdl4239@gmail.com>
2024-12-16 10:53:34 +0900

    feat : 예외처리 검사하는 클래스 생성

---
+++ src/main/java/com/example/schedulejpa/utils/Check.java
@@ -0,0 +1,28 @@
+package com.example.schedulejpa.utils;
+
+import com.example.schedulejpa.config.PasswordEncoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Objects;
+
+@Component
+@RequiredArgsConstructor
+public class Check {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public void checkSameId(Long id1, Long id2, String warning) {
+        if (!Objects.equals(id1, id2)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, warning);
+        }
+    }
+
+    public void checkSamePw(String enteredPw, String foundPw, String warning) {
+        if (!passwordEncoder.matches(enteredPw, foundPw)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, warning);
+        }
+    }
+}
