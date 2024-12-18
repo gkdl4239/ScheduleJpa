e6df574c9aea54f08791e3f68b7e1b803b6b810c
gkdl4239 <gkdl4239@gmail.com>
2024-12-19 01:00:01 +0900

    fix : 서비스 단에서 N+1 로 쿼리문을 불러오는 구조 jpa에서 조인으로 쿼리문을 직접 명시함으로써 수정

--- src/main/java/com/example/schedulejpa/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedulejpa/repository/ScheduleRepository.java
@@ -1,9 +1,16 @@
 package com.example.schedulejpa.repository;
 
+import com.example.schedulejpa.dto.ScheduleResponseDto;
 import com.example.schedulejpa.entity.Schedule;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
@@ -15,4 +22,11 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
                                 "존재하지 않는 일정입니다."
                         ));
     }
+
+    @Query( value= "SELECT new com.example.schedulejpa.dto.ScheduleResponseDto" +
+            "(s.id,s.user.username,s.title,s.contents,s.createdAt,s.updatedAt, COUNT(c)) " +
+            "FROM Schedule s " +
+            "LEFT JOIN Comment c ON c.schedule.id = s.id " +
+            "GROUP BY s")
+    Page<ScheduleResponseDto> findAllScheduleAndCommentCount(Pageable pageable);
 }
