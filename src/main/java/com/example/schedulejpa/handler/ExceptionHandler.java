3388cd756fbd24f7a87e35c50fdd33fcf42ac353
gkdl4239 <gkdl4239@gmail.com>
2025-01-03 16:22:23 +0900

    refactor : Custom 예외처리로 변경

--- src/main/java/com/example/schedulejpa/handler/ExceptionHandler.java
+++ src/main/java/com/example/schedulejpa/handler/ExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.schedulejpa.handler;
 
 import com.example.schedulejpa.config.PasswordEncoder;
+import com.example.schedulejpa.exception.CustomBadRequestException;
+import com.example.schedulejpa.exception.CustomNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -16,13 +18,13 @@ public class ExceptionHandler {
 
     public void checkSameId(Long id1, Long id2, String warning) {
         if (!Objects.equals(id1, id2)) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, warning);
+            throw new CustomBadRequestException(warning);
         }
     }
 
     public void checkSamePw(String enteredPw, String foundPw, String warning) {
         if (!passwordEncoder.matches(enteredPw, foundPw)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, warning);
+            throw new CustomBadRequestException(warning);
         }
     }
 }
