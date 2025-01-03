8b5df0612236b37cfc4d6797928b2098234d1ff7
gkdl4239 <gkdl4239@gmail.com>
2025-01-03 16:27:04 +0900

    feat : 정적 팩토리 메서드 생성

--- src/main/java/com/example/schedulejpa/dto/SignUpResponseDto.java
+++ src/main/java/com/example/schedulejpa/dto/SignUpResponseDto.java
@@ -1,16 +1,19 @@
 package com.example.schedulejpa.dto;
 
 
+import com.example.schedulejpa.entity.User;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
-public class SignUpResponseDto {
-
-    private final Long id;
-
-    private final String username;
-
-    private final String email;
+public record SignUpResponseDto(
+        Long id,
+        String username,
+        String email) {
+    public static SignUpResponseDto toDto(User user) {
+        return new SignUpResponseDto(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail()
+        );
+    }
 }
