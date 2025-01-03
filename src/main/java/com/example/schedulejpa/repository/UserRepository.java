117bd585cf05f3dd5209f66b7f72800b8aca8a70
gkdl4239 <gkdl4239@gmail.com>
2025-01-03 16:27:36 +0900

    refactor : Service로 예외 이동

--- src/main/java/com/example/schedulejpa/repository/UserRepository.java
+++ src/main/java/com/example/schedulejpa/repository/UserRepository.java
@@ -2,21 +2,10 @@ package com.example.schedulejpa.repository;
 
 import com.example.schedulejpa.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
-
-    default User findByIdOrElseThrow(Long id) {
-        return findById(id)
-                .orElseThrow(() ->
-                        new ResponseStatusException(
-                                HttpStatus.NOT_FOUND,
-                                "존재하지 않는 유저 입니다."
-                        ));
-    }
 }
