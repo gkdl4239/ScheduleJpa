11ec8d4877178fa43bf11be274fa0a6d05f442c2
gkdl4239 <gkdl4239@gmail.com>
2025-01-03 16:23:28 +0900

    feat : ErrorResponse 사용 및 custom예외 메서드 추가

--- src/main/java/com/example/schedulejpa/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/schedulejpa/exception/GlobalExceptionHandler.java
@@ -1,34 +1,61 @@
 package com.example.schedulejpa.exception;
 
+import com.example.schedulejpa.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
+
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // Validation 예외 처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException e) {
-        Map<String, String> error = new HashMap<>();
-        FieldError firstError = e.getBindingResult().getFieldErrors().stream().findFirst().orElseThrow();
-        error.put(firstError.getField(), firstError.getDefaultMessage());
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+         ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "BAD_REQUEST",
+                e.getMessage()
+        );
 
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
-    // Object 예외 처리
-    @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException e) {
-        Map<String, String> error = new HashMap<>();
-        error.put("error", e.getReason());
-        return new ResponseEntity<>(error, e.getStatusCode());
+    @ExceptionHandler(CustomBadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleCustomBadRequestException(CustomBadRequestException e) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "BAD_REQUEST",
+                e.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CustomNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCustomNotFoundException(CustomNotFoundException e) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "NOT_FOUND",
+                e.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(CustomUnauthorizedException.class)
+    public ResponseEntity<ErrorResponse> CustomUnauthorizedException(CustomUnauthorizedException e) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.UNAUTHORIZED.value(),
+                "UNAUTHORIZED",
+                e.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
     }
 }
