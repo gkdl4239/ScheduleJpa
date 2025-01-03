064ce702b078705436798a4ca383932fd14c7d9f
gkdl4239 <gkdl4239@gmail.com>
2025-01-03 16:25:18 +0900

    refactor : Service로 예외처리 이전

--- src/main/java/com/example/schedulejpa/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedulejpa/repository/ScheduleRepository.java
@@ -6,22 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    default Schedule findByIdOrElseThrow(Long id) {
-        return findById(id)
-                .orElseThrow(() ->
-                        new ResponseStatusException(
-                                HttpStatus.NOT_FOUND,
-                                "존재하지 않는 일정입니다."
-                        ));
-    }
 
     @Query(value = "SELECT new com.example.schedulejpa.dto.ScheduleResponseDto" +
             "(s.id,s.user.username,s.title,s.contents,s.createdAt,s.updatedAt, COUNT(c)) " +
