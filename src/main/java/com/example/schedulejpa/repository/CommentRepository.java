3524b08843c645686ef0797450a4593c43d59e81
gkdl4239 <gkdl4239@gmail.com>
2025-01-03 16:19:22 +0900

    refactor : Service 단으로 예외 처리 이동

--- src/main/java/com/example/schedulejpa/repository/CommentRepository.java
+++ src/main/java/com/example/schedulejpa/repository/CommentRepository.java
@@ -3,8 +3,6 @@ package com.example.schedulejpa.repository;
 import com.example.schedulejpa.entity.Comment;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -13,15 +11,5 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     long countByScheduleId(Long scheduleId);
 
     List<Comment> findByScheduleId(Long scheduleId, Sort sort);
-
-    default Comment findByIdOrElseThrow(Long id) {
-        return findById(id)
-                .orElseThrow(() ->
-                        new ResponseStatusException(
-                                HttpStatus.NOT_FOUND,
-                                "존재하지 않는 댓글 입니다."
-                        ));
-    }
-
 
 }
